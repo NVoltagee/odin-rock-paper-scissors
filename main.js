e92c40624bea10b47e2078a3014b879039947c79
Norman Trujillo <natrujillo9@gmail.com>
2022-08-26 15:58:06 -0400

    Add initial variables and function

---
+++ main.js
@@ -0,0 +1,9 @@
+const playerScore = 0
+const computerScore = 0
+const roundWinner = ""
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        roundWinner = 'tie'
+    }
+  }
