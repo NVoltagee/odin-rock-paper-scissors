33de1169fff473ccbd756be722b2b24bd4d92204
Norman Trujillo <natrujillo9@gmail.com>
2022-08-26 16:34:07 -0400

    Add computer turn function and game over function

--- main.js
+++ main.js
@@ -1,9 +1,28 @@
-const playerScore = 0
-const computerScore = 0
-const roundWinner = ""
+const playerScore = 0;
+const computerScore = 0;
+const roundWinner = "";
 
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) {
-        roundWinner = 'tie'
-    }
+  if (playerSelection === computerSelection) {
+    roundWinner = "tie";
   }
+}
+
+// Computer selection
+function computerTurn() {
+  const randomNumber = Math.floor(Math.random() * 3) + 1; //Randomizes computer selection from 1 to 3
+  switch (randomNumber) {
+    case 0:
+      return "ROCK";
+    case 1:
+      return "PAPER";
+    case 2:
+      return "Scissors";
+  }
+}
+
+// Declares winner of the match
+
+function gameOver() {
+  return playerScore === 5 || computerScore === 5;
+}
