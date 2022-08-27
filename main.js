748c1de521bc7974777a6548a14b46019c9bb373
Norman Trujillo <natrujillo9@gmail.com>
2022-08-26 20:39:43 -0400

    Add game function

--- main.js
+++ main.js
@@ -8,6 +8,22 @@ function playRound(playerSelection, computerSelection) {
   }
 }
 
+// Game
+function playRound(playerSelection, computerSelection) {
+  if (
+    selection !== "rock" &&
+    selection !== "paper" &&
+    selection !== "scissors"
+  ) {
+    alert(
+      "You've entered an incorrect selection!",
+      "You need to enter either rock, paper or scissors"
+    );
+  } else {
+    return results(selection, computerPlay());
+  }
+}
+
 // Computer selection
 function computerTurn() {
   const randomNumber = Math.floor(Math.random() * 3) + 1; //Randomizes computer selection from 1 to 3
@@ -22,7 +38,6 @@ function computerTurn() {
 }
 
 // Declares winner of the match
-
 function gameOver() {
   return playerScore === 5 || computerScore === 5;
 }
