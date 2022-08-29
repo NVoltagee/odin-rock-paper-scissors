92e189ff1cf7240e2483d72073480e9655fad69c
Norman Trujillo <natrujillo9@gmail.com>
2022-08-29 11:58:21 -0400

    Add Game function

--- main.js
+++ main.js
@@ -10,19 +10,23 @@ function playRound(playerSelection, computerSelection) {
 
 // Game
 function playRound(playerSelection, computerSelection) {
-  if (
-    selection !== "rock" &&
-    selection !== "paper" &&
-    selection !== "scissors"
-  ) {
-    alert(
-      "You've entered an incorrect selection!",
-      "You need to enter either rock, paper or scissors"
-    );
-  } else {
-    return results(selection, computerPlay());
+  if (playerSelection === computerSelection){
+    return `It's a tie! you both picked ${playerSelection}`;
+  } else if (playerSelection === "rock" && computerSelection === "scissors") {
+    return "You Win! Rock beats Scissors";
+  } else if (playerSelection === "paper" && computerSelection === "rock") {
+    return "You Win! Paper beats Rock";
+  } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    return "You Win! Scissors beats Paper";
+  }
+   else {
+    return `You lose! ${computerSelection} beats ${playerSelection}`;
   }
 }
+
+const playerSelection = prompt("Start the game by picking among 'Rock, Paper, Scissors'").toLowerCase();
+const computerSelection = computerTurn();
+console.log(playRound(playerSelection, computerSelection))
 
 // Computer selection
 function computerTurn() {
